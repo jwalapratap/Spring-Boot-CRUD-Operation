6a40be93258f55ac1416699397e6696ce6a71a4f
Jwala Pratap Singh Goutam <79332044+jwalaP@users.noreply.github.com>
2021-06-15 19:32:00 +0530

    first commit

---
+++ src/main/java/com/studentcrud/demo/repository/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.studentcrud.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.studentcrud.demo.domain.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+	
+	
+
+}
